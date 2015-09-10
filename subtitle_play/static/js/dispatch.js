f1f4bb2c38326275297f975c88d9f2d8aaf29390
TheBurn <sunminlei@gmail.com>
2015-09-10 22:00:57 +0800

    update html dispath

--- subtitle_play/static/js/dispatch.js
+++ subtitle_play/static/js/dispatch.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 	$("#config").on("click",function(){
-        window.location.replace("http://www.baidu.com")
+        window.location.replace("http://172.18.14.100/config/")
+    });
+
+	$("#controller").on("click",function(){
+        window.location.replace("http://172.18.14.100/controller/")
+    });
+
+	$("#show").on("click",function(){
+        window.location.replace("http://172.18.14.100/show/")
     });
 });
