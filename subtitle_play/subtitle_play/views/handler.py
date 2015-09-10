9e9dd31e58b3532caa3af8b7852f3c03c3412a87
TheBurn <sunminlei@gmail.com>
2015-09-10 22:25:43 +0800

    update html dispatch

--- subtitle_play/subtitle_play/views/handler.py
+++ subtitle_play/subtitle_play/views/handler.py
@@ -24,6 +24,9 @@ def __log_test(msg):
 def config(request):
     return render_to_response("config.html")
 
+def controller(request):
+    return render_to_response("config.html")
+
 def show(request):
     return render_to_response("show.html")
 
