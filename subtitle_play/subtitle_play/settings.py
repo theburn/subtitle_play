1af61c55956942c7c9d042b4478a4011367e2495
TheBurn <sunminlei@gmail.com>
2015-09-11 15:21:50 +0800

    update

--- subtitle_play/subtitle_play/settings.py
+++ subtitle_play/subtitle_play/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'subtitle_play.music'
+    'subtitle_play.music',
     'gunicorn',
 )
 
