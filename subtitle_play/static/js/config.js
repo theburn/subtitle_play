102e245d6ffd8f556fcf4ac736736937e7c48d6c
TheBurn <sunminlei@gmail.com>
2015-09-11 17:28:09 +0800

    update

--- subtitle_play/static/js/config.js
+++ subtitle_play/static/js/config.js
@@ -26,4 +26,36 @@ $(document).ready(function() {
         },"json")
 
     });
+
+
+
+    $("#mv_upload").on("click",function(){
+        $.get("/get_subtitle_template/",
+            function(data){
+                var result = data.result;
+                var mv_template = "Empty"
+                if (result == 0) {
+                    mv_template = data.mv_list;
+                }
+                //clean html content first
+                $("#subtitle_list_template > ul").empty();
+                //for get mv list
+                if (mv_template != "Empty") {
+                    for(var i = 0; i < mv_template.length; i++) {
+                        var content = "<li>" + mv_template[i] + "</li>";
+                        $("#subtitle_list_template > ul").append(content);
+
+                    }
+                } else {
+                    $("#subtitle_list_template > ul").html(mv_template);
+                }
+
+        },"json")
+
+    });
+
+
+
+
+
 });
