f91b5aded7f50016f3bd05de21531fde5f7bfab9
TheBurn <sunminlei@gmail.com>
2015-09-11 13:00:00 +0800

    add upload handler

--- subtitle_play/subtitle_play/views/handler.py
+++ subtitle_play/subtitle_play/views/handler.py
@@ -7,11 +7,15 @@ from django.http import JsonResponse,  HttpResponseRedirect
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render_to_response, redirect
-import json
 from django.contrib.auth.decorators import login_required
+from subtitle_play import settings
+import json
 import time
-
+import xml.sax.saxutils
 import logging
+
+MAX_SIZE = 1024 * 1024 * 1024 * 2   # 2G
+UPLOAD_PATH = settings.MEDIA_ROOT
 
 from common_function import get_ip_address
 
@@ -32,4 +36,71 @@ def show(request):
 def dispatch(request):
     return render_to_response("dispatch.html", \
                                 {"IP" : get_ip_address("eth0")})
+
+
+
+def upload_file(request, target):
+    res_upload = {"result": None}
+    log_msg = {\
+               "0":u"上传成功", \
+               "1":u"上传文件已存在", \
+               "2":u"磁盘IO错误", \
+               "3":u"异常错误", \
+               "4":u"HTTP方法错误", \
+               "5":u"上传文件过大", \
+               "10":u"上传文件已存在", \
+            }
+    name = "None"
+    target_flag = True
+    if request.method=='POST':
+        try:
+            if target == "mv":
+                UPLOAD_PATH = os.path.join([UPLOAD_PATH, "mv_templates"])
+            elif target == "subtitle":
+                UPLOAD_PATH = os.path.join([UPLOAD_PATH, "subtitle_templates"])
+            else:
+                target_flag = False
+
+
+            if target_flag:
+                f = request.FILES.get('uploadfile')
+                name = xml.sax.saxutils.escape(f.name).strip().replace(" ", "_").encode("utf-8")
+
+                if not os.path.exists(UPLOAD_PATH):
+                    os.makedirs(UPLOAD_PATH)
+
+                filename = os.path.join(UPLOAD_PATH, name)
+
+                if os.path.exists(filename):
+                    os.remove(filename) # 先删除老的文件
+
+                if f.size > MAX_SIZE:
+                    res_upload["result"] = 5
+                else:
+                    with open(filename, 'wb+') as tempfile:
+                        for chunk in f.chunks():
+                            tempfile.write(chunk)
+                    
+                    #TODO
+
+                    res_upload["result"] = 0
+            else:
+                res_upload["result"] = 10
+        except IOError:
+            res_upload["result"] = 2
+        except:
+            res_upload["result"] = 3
+    else:
+        res_upload["result"] = 4
+
+
+    log_event = u"上传" + name
+    res_key = str(res_upload['result'])
+    log = logging.getLogger("audit")
+    log.debug("%s %s %s %s",  \
+            request.session['ip'],  request.session['user_name'],  \
+            log_event,  log_msg[res_key]) 
+
+    return JsonResponse(res_upload)
+
 
