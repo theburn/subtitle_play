bdd65a409ae38d1600e1c53c49599611b903eb61
TheBurn <sunminlei@gmail.com>
2015-09-12 22:18:57 +0800

    add upload js

--- subtitle_play/static/js/config.js
+++ subtitle_play/static/js/config.js
@@ -57,7 +57,7 @@ $(document).ready(function() {
     $('#fileupload').fileupload({
         dataType: 'json',
         formData: [
-            { name: "csrfmiddlewaretoken", value:getCookie('csrftoken')}
+            { name: "csrfmiddlewaretoken", value:get_cookie('csrftoken')}
         ],
 
 
@@ -92,3 +92,21 @@ $(document).ready(function() {
 
 
 });
+
+
+
+function get_cookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
