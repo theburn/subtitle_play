61ed792b779e01e5ee8ca32b8121a9cdc0424c49
TheBurn <sunminlei@gmail.com>
2015-09-14 16:00:10 +0800

    update

--- subtitle_play/subtitle_play/urls.py
+++ subtitle_play/subtitle_play/urls.py
@@ -26,6 +26,7 @@ urlpatterns += patterns('subtitle_play.views',
     url(r'^show/$',  'show'), 
     url(r'^get_mv_template/$',  'get_mv_template'), 
     url(r'^get_subtitle_template/$',  'get_subtitle_template'), 
+    url(r'^post_music/$',  'post_music'), 
 )
 
 
