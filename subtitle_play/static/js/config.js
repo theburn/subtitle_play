1412020659ed0d15a6281958bf2386458ed722d9
TheBurn <sunminlei@gmail.com>
2015-09-14 16:59:11 +0800

    update

--- subtitle_play/static/js/config.js
+++ subtitle_play/static/js/config.js
@@ -235,8 +235,8 @@ $(document).ready(function() {
             {
                 //music_name:$("#music_dropdown_selected").val(),
                 music_name:$("#search_text").val(),
-                mv_name:$("#mv_dropdown_selected").val(),
-                subtitle_name:$("#subtitle_dropdown_selected").val(),
+                mv_name:$("#mv_selected").val(),
+                subtitle_name:$("#subtitle__selected").val(),
                 csrfmiddlewaretoken:get_cookie('csrftoken'),
             },
             function(data){
