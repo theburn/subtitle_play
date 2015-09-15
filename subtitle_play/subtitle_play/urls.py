a03d0c92436a31275e4c265ad6034856942b8a33
TheBurn <sunminlei@gmail.com>
2015-09-15 12:38:12 +0800

    Tue Sep 15 12:38:12 CST 2015

--- subtitle_play/subtitle_play/urls.py
+++ subtitle_play/subtitle_play/urls.py
@@ -30,7 +30,7 @@ urlpatterns += patterns('subtitle_play.views',
     url(r'^delete_music/$',  'delete_music'), 
     url(r'^get_music_list/$',  'get_music_list'), 
     url(r'^get_music_args/$',  'get_music_args'), 
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
